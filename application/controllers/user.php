98cebd8f09fe3ea3c00e00d19b6a428cd3b62cca
Allister Antosik <allistera@gmail.com>
2013-03-11 19:14:55 +0000

    Created user controller

---
+++ application/controllers/user.php
@@ -0,0 +1,22 @@
+<?php
+
+class User_Controller extends Base_Controller {
+
+	public $restful = true;    
+
+	public function get_login()
+    {
+
+    }
+
+    public function post_login()
+    {
+
+    }
+
+    public function get_logout()
+    {
+
+    }
+
+}
